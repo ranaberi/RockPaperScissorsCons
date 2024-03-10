d8d9bd353bde72f0db6693801d14ebe206e5ba3c
Rana <ranaberi25@gmail.com>
2024-03-10 20:57:18 +0100

    R beats S Test

---
+++ RockPaperScissorsConsTests/ForcedPlayer.cs
@@ -0,0 +1,16 @@
+﻿using RockPaperScissorsCons;
+
+namespace RockPaperScissorsConsTests;
+
+public class ForcedPlayer: IPlayer
+{
+    private Choice _choice;
+    public ForcedPlayer(Choice choice)
+    {
+        _choice = choice;
+    }
+    public Choice GetChoice()
+    {
+        return _choice;
+    }
+}
---
+++ RockPaperScissorsConsTests/RPSTests.cs
@@ -0,0 +1,14 @@
+﻿
+using RockPaperScissorsCons;
+
+namespace RockPaperScissorsConsTests;
+
+public class RPSTests
+{
+    [Test]
+    public void RockBeatsScissors()
+    {
+        GameManager gm = new GameManager(new ForcedPlayer(Choice.Rock), new  ForcedPlayer(Choice.Scissors));
+        Assert.That(gm.PlayRound(), Is.EqualTo(RoundResult.Player1Win));
+    }
+}
